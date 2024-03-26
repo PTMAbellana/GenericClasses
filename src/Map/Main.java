e7b25dd103e634f686aa65f9b2a57642da856b23
PAUL ABELLANA <paulmabellana@gmail.com>
2024-03-26 22:42:57 +0800

    main testing

--- src/Map/Main.java
+++ src/Map/Main.java
@@ -1,17 +1,46 @@
 package Map;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        MyMap<String, Integer> myMap = new MyMap<>();
-        myMap.put("one", 1);
-        myMap.put("two", 2);
-        myMap.put("three", 3);
+        Scanner sc = new Scanner(System.in);
+        MyMap<Integer, String> map = new MyMap<>();
+        int key;
+        String value;
 
-        System.out.println("Value for key 'one': " + myMap.get("one"));
-        System.out.println("Value for key 'two': " + myMap.get("two"));
-        System.out.println("Value for key 'four': " + myMap.get("four"));
+        System.out.println();
+        System.out.println("PUT METHOD TEST CASES: ");
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(3, "Three");
+        map.put(4, "Four");
+        map.put(5, "Five");
+//        map.put(1, "One - Copy");
+//        map.put(1, "One");
 
-        System.out.println("Removing key 'two', value: " + myMap.remove("two"));
-        System.out.println("Value for key 'two' after removal: " + myMap.get("two"));
+        System.out.println("CURRENT KEYS AND VALUES: ");
+        System.out.println(map.keys);
+        System.out.println(map.values);
+
+        System.out.println();
+        System.out.println("GET METHOD TEST CASES: ");
+        System.out.println("Value of key 3: " + map.get(3));
+        System.out.println("Value of key 5: " + map.get(5));
+        System.out.println("Value of key 23: " + map.get(23));//dapat null
+        System.out.println("Value of key 1: " + map.get(1));
+        System.out.println("Value of key 20: " + map.get(20)); //Dapat null
+
+        System.out.println();
+        System.out.println("REMOVE METHOD TEST CASES: ");
+        System.out.println("Removed value from key 2: " + map.remove(2));
+        System.out.println("Current Keys and Values:");
+        System.out.println(map.keys);
+        System.out.println(map.values);
+
+        System.out.println("Removed value from key 5: " + map.remove(5));
+        System.out.println("Current Keys and Values:");
+        System.out.println(map.keys);
+        System.out.println(map.values);
     }
 }
--- src/Map/MyMap.java
+++ src/Map/MyMap.java
@@ -2,8 +2,8 @@ package Map;
 import java.util.ArrayList;
 
 public class MyMap<K, V> {
-    private ArrayList<K> keys;
-    private ArrayList<V> values;
+    ArrayList<K> keys; //package-priv default
+    ArrayList<V> values;
 
     public MyMap() {
         keys = new ArrayList<>();
@@ -11,8 +11,8 @@ public class MyMap<K, V> {
     }
 
     public void put(K key, V value) {
-        int index = keys.indexOf(key);
-        if (index != -1) {
+        int index = keys.indexOf(key); // instead of using for loops nga magcreate pud ug para lain static entry class
+        if (index != -1) { // if naa ang key
             values.set(index, value);
         } else {
             keys.add(key);
